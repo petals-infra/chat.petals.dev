10f853e44bc0da63cc21e87667d9b0e4bd9bf7b5
Aleksandr Borzunov <borzunov.alexander@gmail.com>
2023-01-15 20:25:06 +0000

    Use WebSocket API

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import threading
 from contextlib import ExitStack, nullcontext
 from traceback import format_exc
@@ -6,6 +7,7 @@ from uuid import uuid4
 import hivemind
 import torch
 from flask import Flask, jsonify, request
+from flask_sock import Sock
 from transformers import BloomTokenizerFast
 
 from petals import DistributedBloomForCausalLM
@@ -41,6 +43,8 @@ inference_sessions = hivemind.TimedStorage()  # Should be used under storage_loc
 
 logger.info("Starting Flask app")
 app = Flask(__name__)
+app.config['SOCK_SERVER_OPTIONS'] = {'ping_interval': 25}
+sock = Sock(app)
 
 
 @app.route("/")
@@ -49,7 +53,7 @@ def main_page():
 
 
 @app.get("/api/v1/open_inference_session")
-def open_inference_session():
+def http_api_open_inference_session():
     try:
         max_length = get_typed_arg("max_length", int, 1024)
 
@@ -78,7 +82,7 @@ def open_inference_session():
 
 
 @app.get("/api/v1/close_inference_session")
-def close_inference_session():
+def http_api_close_inference_session():
     try:
         session_id = request.values.get("session_id")
 
@@ -91,7 +95,7 @@ def close_inference_session():
 
 
 @app.post("/api/v1/generate")
-def generate():
+def http_api_generate():
     try:
         inputs = request.values.get("inputs")
         do_sample = get_typed_arg("do_sample", int, 0)
@@ -145,3 +149,55 @@ def generate():
 def get_typed_arg(name, expected_type, default=None):
     value = request.values.get(name, default)
     return expected_type(value) if value is not None else None
+
+
+@sock.route("/api/v2/generate")
+def ws_api_generate(ws):
+    try:
+        request = json.loads(ws.receive(timeout=SESSION_EXPIRATION))
+        assert request["type"] == "open_inference_session"
+        logger.info(f"ws.generate.open(), max_length={request['max_length']}")
+
+        with model.inference_session(max_length=request["max_length"]) as session:
+            ws.send(json.dumps({"ok": True}))
+
+            while True:
+                request = json.loads(ws.receive(timeout=SESSION_EXPIRATION))
+                assert request["type"] == "generate"
+                inputs = request.get("inputs")
+                logger.info(f"ws.generate.step(), inputs={repr(inputs)}")
+
+                if inputs is not None:
+                    inputs = tokenizer(inputs, return_tensors="pt")["input_ids"].to(DEVICE)
+                    n_input_tokens = inputs.shape[1]
+                else:
+                    n_input_tokens = 0
+
+                stop_sequence = request.get("stop_sequence")
+                all_outputs = ''
+                stop = False
+                while not stop:
+                    outputs = model.generate(
+                        inputs=inputs,
+                        do_sample=request.get("do_sample", False),
+                        temperature=request.get("temperature", 1.0),
+                        top_k=request.get("top_k"),
+                        top_p=request.get("top_p"),
+                        max_length=request.get("max_length"),
+                        max_new_tokens=request.get("max_new_tokens"),
+                        session=session,
+                    )
+                    outputs = tokenizer.decode(outputs[0, n_input_tokens:])
+                    all_outputs += outputs
+
+                    stop = stop_sequence is None or stop_sequence in all_outputs
+                    inputs = None  # Inputs are passed only for the 1st token of the bot's response
+                    n_input_tokens = 0
+
+                    logger.info(f"ws.generate.step(), all_outputs={repr(all_outputs)}, stop={stop}")
+                    ws.send(json.dumps({"ok": True, "outputs": outputs, "stop": stop}))
+    except Exception:
+        logger.warning("ws.generate failed:", exc_info=True)
+        ws.send(json.dumps({"ok": False, "traceback": format_exc()}))
+    finally:
+        logger.info(f"ws.generate.close()")
