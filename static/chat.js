dacf286f0153c8ea0c840f563e7966f2ab80e57b
Tijs Zwinkels <tijs@tinkertank.eu>
2023-02-21 23:34:58 +0100

    ws close suggestion from @borzunov
    Co-authored-by: Alexander Borzunov <hxrussia@gmail.com>

--- static/chat.js
+++ static/chat.js
@@ -165,7 +165,7 @@ function receiveReplica(inputs) {
       }
     } else {
       $('.loading-animation, .speed, .suggest-join, .generation-controls').remove();
-      ws.close();
+      resetSession();
       appendTextArea();
       stop = false;
     }
