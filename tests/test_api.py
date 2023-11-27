01792e0d544786a0ae1c104fb12b5ae5cab67691
Roman Ageev <ageevroma2000@gmail.com>
2023-11-27 23:01:30 +0800

    fix: tests replace

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,13 +3,14 @@ import requests
 
 def test_api():
     request = {
+        "model": "petals-team/StableBeluga2",
         "prompt": "A cat sat on a mat",
-        "stream": False,
-        "max_new_tokens": 4,
+        "max_new_tokens": 20,
     }
-    response = requests.post("http://localhost:8000/generate", json=request)
+    response = requests.post("http://localhost:8000/v1/completions", json=request)
     assert response.status_code == 200
     response.raise_for_status()
-    assert "response" in response.json(), "Response should contain a 'response' field"
-    assert isinstance(response.json()["response"], str), "Response should be a string"
-    assert len(response.json()["response"]) > len(request["prompt"]), "Response should be longer than the prompt"
+    assert "choices" in response.json(), "Response should contain a 'choices' field"
+    assert isinstance(response.json()["choices"], list), "Choices should be a list"
+    print("API test passed")
+    print("Response:", response.json())
